a88951767cdc67d64bd777f4313756e2a1ca89b5
Gustavo Canello <gustavobrandicanello@gmail.com>
2024-12-04 11:02:00 -0300

    remove body form

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import { UserInterface } from "./screen/user-interface.js";
-import { assembleBody } from "./world/assemble-body.js";
 import { Piece, Wave } from "./world/form.js";
 import { World } from "./world/world.js";
 
 const world = new World();
 new UserInterface();
-world.addForm(assembleBody());
 world.addForm(new Piece(20, 20, 800, 400));
 world.addForm(new Piece(1188, 0, 100, 100));
 world.addForm(new Piece(0, 768, 100, 100));
